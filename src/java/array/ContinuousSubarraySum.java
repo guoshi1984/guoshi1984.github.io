f251a2fd224132595e4d6f1f1d0d19833f57d246
shi <guoshi1984@hotmail.com>
2020-05-02 11:14:24 -0700

    add subarray directory

--- src/java/array/ContinuousSubarraySum.java
+++ src/java/array/ContinuousSubarraySum.java
@@ -1,37 +1,28 @@
-public class ContinuousSubarraySum
+import java.util.*;
+public class ContinuousSubarraySum1
 {
-	public static boolean check(int[] nums, int k)
+	public static boolean find(int[] nums, int k)
 	{
-		final int n = nums.length;
-		int cur = nums[0];
-		int i = 0;
-		int j = 0;
-		
-		while(true)
+		Map<Integer, Integer> map = new HashMap<Integer,
+		Integer>(){{put(0, -1);}};
+	
+		int runningSum = 0;
+		for(int i=0; i<nums.length; i++)
 		{
-			System.out.println(cur + " "+ i + " "+ j);
-			if(cur == k) return true;
-			else if(cur < k)
-			{	
-				if(j == n-1) return false;
-				j++;
-				cur = cur + nums[j]; 
-			}
-			else
-			{	
-				if(i == n-1) return false;
-				cur = cur - nums[i];
-				i++;
-			}
-			
-		}
+			runningSum += nums[i];
+			if(k != 0) runningSum %= k;
+			Integer prev = map.get(runningSum);
+			if(prev != null)
+				if(i - prev > 1) return true;
+			else map.put(runningSum, i);
 		
+		}
+		return false;
 	}
 	
 	public static void main(String[] args)
 	{
 		int[] nums = {23, 2, 6, 4, 7};
-		int k = 13;
-		System.out.println(check(nums, k));
+		System.out.println(find(nums, 6));	
 	}
 }
